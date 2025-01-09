f3d516f4bfee61b3a9125a6774c5a4234d9d1a93
AsBorn007 <pk175040@gmai.com>
2025-01-09 17:53:33 +0530

    to find mili or secons

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ document.getElementById("DateObjectString").innerHTML = newDate.toDateString();
 document.getElementById("DateObjectUTCstring").innerHTML = newDate.toLocaleTimeString();
 
 
-let newDateMathod = new Date ();
+let newDateMathod = new Date (2025 , 0 , 9);
 console.log(newDateMathod.toDateString())
-console.log(newDateMathod.toLocaleString())
+console.log(newDateMathod.toLocaleTimeString())
+
+
+let milisecons = Date.now();
+console.log(milisecons)
+
+let seconds =  Date.now();
+let findSecons = Math.floor(seconds/1000);
+console.log(findSecons)

ee415026d9c79115ba9c4128d8e5a4b22d0c8a07
AsBorn007 <pk175040@gmai.com>
2025-01-09 17:33:56 +0530

    new method try

--- main.js
+++ main.js
@@ -2,4 +2,9 @@ const newDate = new Date();
 document.getElementById("mainDate").innerHTML = newDate;
 document.getElementById("DateObject").innerHTML = newDate.toUTCString();
 document.getElementById("DateObjectString").innerHTML = newDate.toDateString();
-document.getElementById("DateObjectUTCstring").innerHTML = newDate.toISOString();
+document.getElementById("DateObjectUTCstring").innerHTML = newDate.toLocaleTimeString();
+
+
+let newDateMathod = new Date ();
+console.log(newDateMathod.toDateString())
+console.log(newDateMathod.toLocaleString())
